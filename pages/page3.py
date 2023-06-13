2e4042b5bbe0c90591602fbed45975f61a966622
GGAM-DAL <114039378+GGAM-DAL@users.noreply.github.com>
2023-06-13 22:36:04 +0900

    Add files via upload

--- pages/page3.py
+++ pages/page3.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import module.database as Database
-import module.Poster as Post
-import module.Graph as Graph
+import module.page as page
 import module.code_list as Codelist
 import module.set_page as Setpage
-import module.Review as Review
-import module.Score as Score
-import module.Graph as Graph
-import pandas as pd
-import module.information as info
+
 
 # setting basic page
 Setpage.Setpage()
@@ -17,32 +12,4 @@ Setpage.Setpage()
 df = Database.db('movie_info')
 code = Codelist.CodeList(df, 0) # Make Code List
 
-
-
-Post.info(df, code[0])  
-
-
-tabs = Setpage.tabs(0)
-# 흥행 기준 설명
-# 카드로 작성
-with tabs[0]:
-    # Show Score
-    info.star()
-    avg_score = Score.cal_score(code[0])
-    Graph.Score(code[0], avg_score)
-with tabs[1]:
-    info.keyword(0)
-    emotion = st.radio("",('긍정 키워드', '부정 키워드'),
-                       horizontal=True)
-    if emotion=='긍정 키워드':
-        emot = 1
-    else:
-        emot = 0
-    Review.show_review(code[0], emot)
-    
-with tabs[2]:
-    info.audience(0)
-    Graph.Audience(code[0], 0)
-with tabs[3]:
-    info.review()
-    Review.review_list(code[0])
+page.pages(df, code[0], 0)
--- pages/page6.py
+++ pages/page6.py
@@ -1,13 +1,9 @@
 import streamlit as st
 import module.database as Database
-import module.Poster as Post
-import module.Graph as Graph
+import module.page as page
 import module.code_list as Codelist
 import module.set_page as Setpage
-import module.Review as Review
-import module.Score as Score
-import module.Graph as Graph
-import module.information as info
+
 
 # setting basic page
 Setpage.Setpage()
@@ -16,32 +12,4 @@ Setpage.Setpage()
 df = Database.db('movie_info')
 code = Codelist.CodeList(df, 1) # Make Code List
 
-
-
-Post.info(df, code[3])  
-
-tabs = Setpage.tabs(1)
-
-with tabs[0]:
-    # Show Score
-    info.star()
-    avg_score = Score.cal_score(code[3])
-    Graph.Score(code[3], avg_score)
-
-with tabs[1]:
-    info.keyword(1)
-    emotion = st.radio("",('긍정 키워드', '부정 키워드'),
-                       horizontal=True)
-    if emotion=='긍정 키워드':
-        emot = 1
-    else:
-        emot = 0
-    Review.show_review(code[3], emot)
-    
-with tabs[2]:
-    info.audience(1)
-    Graph.Audience(code[3], 1)
-
-with tabs[3]:
-    info.review()
-    Review.review_list(code[3])
+page.pages(df, code[3], 1)
